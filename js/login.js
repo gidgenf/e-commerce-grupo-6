70d2a43375332790413521c3b0ba30910945c9c2
gidgenf <gidgenf@hotmail.com>
2023-08-17 15:30:58 -0300

    cambios en index y login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,6 @@
 sesionIni.addEventListener('click', () => {
-    const usuario = document.getElementById('usuario').value
-    const contraseña = document.getElementById('contraseña').value
+    let usuario = document.getElementById('usuario').value
+    let contraseña = document.getElementById('contraseña').value
     if (!contraseña || !usuario) {
 
     } else {
@@ -15,26 +15,28 @@ y luego atravez de un if en el caso de que falten la contraseña o
 el usuario, el boton sesionIni no responderá, y en caso positivo
 redireccionará a index.html
 */
-document.addEventListener("DOMContentLoaded", function() {
-    // Verifica si el usuario NO ha iniciado sesión
+document.addEventListener("DOMContentLoaded", function () {
+    // Verifica si el usuario NO ha iniciado sesión y en caso opuesto si este está iniciado
     if (localStorage.getItem('authenticated') !== 'true') {
         window.location.href = "login.html"; // Redirecciona al usuario a login.html si NO ha iniciado sesión
     }
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Verifica si el usuario ya ha iniciado sesión
-    if (localStorage.getItem('authenticated') === 'true') {
-        window.location.href = "index.html"; // Redirecciona al usuario a index.html si ya ha iniciado sesión
-    }
+// Evento de click para el botón de inicio de sesión
+document.getElementById('sesionIni').addEventListener('click', function () {
 
-    // Evento de click para el botón de inicio de sesión
-    document.getElementById('sesionIni').addEventListener('click', function() {
+    // Si el inicio de sesión es exitoso, guarda la información de la sesión
+    localStorage.setItem('authenticated', 'true');
 
-        // Si el inicio de sesión es exitoso, guarda la información de la sesión
-        localStorage.setItem('authenticated', 'true');
+    // Redireccionar al usuario a index.html después del inicio de sesión exitoso
+    window.location.href = "index.html";
+});
 
-        // Redireccionar al usuario a index.html después del inicio de sesión exitoso
-        window.location.href = "index.html";
-    });
+document.getElementById('cerrarSesion').addEventListener('click', function () {
+
+    // Limpia todo lo existente en local storage
+    localStorage.removeItem('authenticated', 'false');
+
+    // Redireccionar al usuario a index.html después de cerrar sesion
+    window.location.href = "login.html";
 });
