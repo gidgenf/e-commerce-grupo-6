02dbbbe4e5912c84b524d94107c05d548f05ee58
gidgenf <gidgenf@hotmail.com>
2023-10-10 14:28:27 -0300

    arreglos en los carritos

---
+++ js/cart.js
@@ -0,0 +1,30 @@
+<<<<<<< Updated upstream
+=======
+document.addEventListener('DOMContentLoaded', () => {
+    const URLidcart = 'https://japceibal.github.io/emercado-api/user_cart/25801.json';
+
+    fetch(URLidcart)
+        .then(response => {
+            return response.json();
+        })
+        .then(cartdata => {
+            localStorage.setItem('usercartprecharged', JSON.stringify(cartdata));
+
+            const storedCartData = JSON.parse(localStorage.getItem('usercartprecharged'));
+
+            const productName = storedCartData.articles[0].name;
+            console.log(productName);
+        })
+        .catch(error => console.error('Error:', error));
+});
+
+
+
+
+
+
+
+
+
+
+>>>>>>> Stashed changes
