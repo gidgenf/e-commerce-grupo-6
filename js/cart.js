a576f3bde202800e871868b81995a6fdb2e74b10
Ian Apud <ianapud23@gmail.com>
2023-10-13 12:46:02 -0300

    rt

--- js/cart.js
+++ js/cart.js
@@ -1,35 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
     const URLidcart = 'https://japceibal.github.io/emercado-api/user_cart/25801.json';
+let localCart = JSON.parse(localStorage.getItem('usercart'));
+let articles = [];
+let cart = document.getElementById('cart')
 
-articles = []
-
-    let carrito = document.getElementById("carrito")
     fetch(URLidcart)
         .then(response => {
             return response.json();
         })
         .then(cartdata => {
+     
+            cartdata.articles.push(...localCart)
+            articles.push(cartdata.articles);
 
-            console.log (cartdata)
-            articles.push(cartdata.articles)
-            console.log(articles)
-            showarticles(articles)
+            console.log(articles);
+            showarticles(articles);
         })
         .catch(error => console.error('Error:', error));
-        
-});
 
-function showarticles(articles){ 
-    carrito.inerHTML=``
-    articles.forEach(element => {
-        carrito.inerHTML+= `<div class="card text-bg-dark">
-<img src="${element.image}" class="card-img" alt="...">
-<div class="card-img-overlay">
-  <h5 class="card-title">Card title</h5>
-  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-  <p class="card-text"><small>Last updated 3 mins ago</small></p>
-</div>
-</div>`
+
+        function showarticles(articles){ 
+            cart.innerHTML=``
+            articles[0].forEach(element => {
+                cart.innerHTML += `
+                <div class="card mt-5 mx-5 text-bg-dark">
+                <img src="${element.image}" class="card-img" alt="...">
+                <div class="card-img-overlay">
+               <h4 class="card-title">${element.name}</h4>
+               <p class="card-text">${element.currency}${element.unitCost}</p>
+               <h5 class="card-text">cantidad:<span class="badge bg-primary rounded-pill">${element.count}</span></h5>
+               </div>
+            </div>`
+            
+
     });
 }
-
+});
