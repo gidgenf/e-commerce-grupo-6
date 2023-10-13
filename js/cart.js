01a0271574c8c2ce1db6d37f7dfcb12deed76d8e
Ian Apud <ianapud23@gmail.com>
2023-10-13 18:51:40 -0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -29,22 +29,21 @@ let cart = document.getElementById('cart')
  <h3 class="card-title">${element.name}</h3>
   </div>
   <div class="card-body">
-    <blockquote class="blockquote mb-0">
     <img src="${element.image}" style="width: 10rem;" >
     <div class="dropdown-center float-end">
     <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
     opciones
     </button>
     <ul class="dropdown-menu">
-      <li><input class="form-check-input me-1" type="radio" name="listGroupRadio" value="" id="firstRadio" checked>
+      <li><input class="form-check-input me-1" type="radio" name="listGroupRadio" value="" checked>
       <label class="form-check-label" for="firstRadio">premium 2 a 5 dias (17%)</label></li>
-      <li><input class="form-check-input me-1" type="radio" name="listGroupRadio" value="" id="firstRadio" checked>
+      <li><input class="form-check-input me-1" type="radio" name="listGroupRadio" value=""  checked>
       <label class="form-check-label" for="firstRadio">express 5 a 8 dias (7%)</label></li>
-      <li><input class="form-check-input me-1" type="radio" name="listGroupRadio" value="" id="firstRadio" checked>
+      <li><input class="form-check-input me-1" type="radio" name="listGroupRadio" value=""  checked>
       <label class="form-check-label" for="firstRadio">standard 8 a 12 dias (5%) </label></li>
     </ul>
 </div>
-   <p class="position-absolute bottom-0 mb-1 mx-1 end-0"card-text">${element.currency}${element.unitCost}</p>
+   <p class="btn btn-success position-absolute bottom-0 m-3 end-0 ">${element.currency}${element.unitCost*element.count }</p>
 </div>
 </div>`
             
