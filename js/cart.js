b0a93733d47b9dcabfe0067a430b13a95f6ec9a7
Ian Apud <ianapud23@gmail.com>
2023-10-14 18:20:19 -0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,9 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const URLidcart = 'https://japceibal.github.io/emercado-api/user_cart/25801.json';
+ const URLidcart = 'https://japceibal.github.io/emercado-api/user_cart/25801.json';
 let localCart = JSON.parse(localStorage.getItem('usercart'));
 let articles = [];
 let cart = document.getElementById('cart')
-
     fetch(URLidcart)
         .then(response => {
             return response.json();
@@ -18,23 +16,42 @@ let cart = document.getElementById('cart')
         })
         .catch(error => console.error('Error:', error));
 
-
         function showarticles(articles){ 
             cart.innerHTML=``
             articles[0].forEach(element => {
                 cart.innerHTML += `
                 <div class="card m-3 shadow">
   <div class="card-header">
- <h5 class="float-end card-text">cantidad:<span class="badge bg-primary rounded-pill">${element.count}</span></h5>
+ <h5 class="float-end card-text" onclick="reloco()">cantidad:<span class="badge bg-primary rounded-pill">${element.count}</span></h5>
  <h3 class="card-title">${element.name}</h3>
   </div>
   <div class="card-body">
     <img src="${element.image}" style="width: 10rem;" >
    <p class="btn btn-success position-absolute bottom-0 m-3 end-0" >Total ${element.currency}${element.unitCost*element.count }</p>
+   <h5 class="float-end card-text">
+    cantidad:
+    <span class="badge bg-primary rounded-pill">${element.count}</span>
+    <button onclick="changeQuantity(${element.id}, true)" type="button" class="btn btn-primary btn-sm">Aumentar</button>
+    <button onclick="changeQuantity(${element.id}, false)" type="button" class="btn btn-danger btn-sm">Disminuir</button>
+</h5>
 </div>
 </div>`
             
 
-    });
-}
+
 });
+}
+function changeQuantity(id, increase) {
+    let product = articles[0].find(item => item.id === id);
+    if (increase) {
+        product.count++;
+    } else {
+        if (product.count > 1) {
+            product.count--;
+        }
+    }
+    showarticles(articles);
+}
+   
+
+
