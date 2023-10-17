0e47abee022a0145e6e1363ba95d1f93f7bf1d35
Ian Apud <ianapud23@gmail.com>
2023-10-17 11:42:47 -0300

    cambio css

--- js/cart.js
+++ js/cart.js
@@ -69,11 +69,9 @@ totalCost= totalCost- totalCost * (valorActivo*0.01);
 cardMoney.innerHTML=`
 <div class="row">
   <div class="col-sm-6">
-    <div class="card">
+    <div class="card" style="width: 18rem;">
       <div class="card-body">
         <h5 class="card-title">total a pagar ${totalCost}</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
       </div>
     </div>
   </div>`
