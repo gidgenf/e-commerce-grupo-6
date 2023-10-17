2c51fc875244d5167cedce633d6aa9d88d90a572
gidgenf <gidgenf@hotmail.com>
2023-10-17 11:18:19 -0300

    a
    a

--- js/cart.js
+++ js/cart.js
@@ -1,23 +1,8 @@
-<<<<<<< HEAD
-document.addEventListener('DOMContentLoaded', () => {
-    const URLidcart = 'https://japceibal.github.io/emercado-api/user_cart/25801.json';
-    let localCart = JSON.parse(localStorage.getItem('usercart'));
-    let articles = [];
-    let cart = document.getElementById('cart')
-
-    fetch(URLidcart)
-        .then(response => {
-            return response.json();
-        })
-        .then(cartdata => {
-
-            cartdata.articles.push(...localCart)
-            articles.push(cartdata.articles);
-=======
 const URLidcart = 'https://japceibal.github.io/emercado-api/user_cart/25801.json';
 let localCart = JSON.parse(localStorage.getItem('usercart'));
 let articles = [];
 let cart = document.getElementById('cart')
+
 fetch(URLidcart)
     .then(response => {
         return response.json();
@@ -26,57 +11,36 @@ fetch(URLidcart)
 
         cartdata.articles.push(...localCart)
         articles.push(cartdata.articles);
->>>>>>> main
 
         console.log(articles);
         showarticles(articles);
     })
     .catch(error => console.error('Error:', error));
 
-<<<<<<< HEAD
-
-    function showarticles(articles) {
-        cart.innerHTML = ``
-        articles[0].forEach(element => {
-            cart.innerHTML += `
-                <div class="card m-3 shadow">
-                    <div class="card-header">
-                        <h5 class="float-end card-text">cantidad:<span class="badge bg-primary rounded-pill"><input type="number" value="${element.count}"></span></h5>
-                        <h3 class="card-title">${element.name}</h3>
-                    </div>
-                    <div class="card-body">
-                        <img src="${element.image}" style="width: 10rem;" >
-                        <p class="btn btn-success position-absolute bottom-0 m-3 end-0" >Total ${element.currency}${element.unitCost * element.count}</p>
-                    </div>
-                </div>`
-
-        });
-    }
-});
-=======
+    
 function showarticles(articles) {
     cart.innerHTML = ``
     articles[0].forEach(element => {
         cart.innerHTML += `
                 <div class="card m-3 shadow">
-  <div class="card-header">
- <h5 class="float-end card-text" onclick="reloco()">cantidad:<span class="badge bg-primary rounded-pill">${element.count}</span></h5>
- <h3 class="card-title">${element.name}</h3>
-  </div>
-  <div class="card-body">
-    <img src="${element.image}" style="width: 10rem;" >
-   <p class="btn btn-success position-absolute bottom-0 m-3 end-0" >Total ${element.currency}${element.unitCost * element.count}</p>
-   <h5 class="float-end card-text">
-    <button onclick="changeQuantity(${element.id}, false)" type="button" class="btn btn-danger btn-sm">Disminuir</button>
-    <button onclick="changeQuantity(${element.id}, true)" type="button" class="btn btn-primary btn-sm">Aumentar</button>
-</h5>
-</div>
-</div>`
-
-
+                    <div class="card-header">
+                        <h5 class="float-end card-text" onclick="reloco()">cantidad:<span class="badge bg-primary rounded-pill">${element.count}</span></h5>
+                        <h3 class="card-title">${element.name}</h3>
+                    </div>
+                    <div class="card-body">
+                        <img src="${element.image}" style="width: 10rem;" >
+                        <p class="btn btn-success position-absolute bottom-0 m-3 end-0" >Total ${element.currency}${element.unitCost * element.count}</p>
+                        <h5 class="float-end card-text">
+                            <button onclick="changeQuantity(${element.id}, false)" type="button" class="btn btn-danger btn-sm">Disminuir</button>
+                            <button onclick="changeQuantity(${element.id}, true)" type="button" class="btn btn-primary btn-sm">Aumentar</button>
+                        </h5>
+                    </div>
+                </div>`
 
     });
 }
+
+
 function changeQuantity(id, value) {
     let product = articles[0].find(item => item.id === id);
     if (value) {
@@ -91,4 +55,3 @@ function changeQuantity(id, value) {
 
 
 
->>>>>>> main
