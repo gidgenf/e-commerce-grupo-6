96f2c796a378117b82f491a0dc4c06db135f7007
gidgenf <gidgenf@hotmail.com>
2023-11-26 16:56:03 -0300

    Revert "reglo cors"
    This reverts commit 327de3ab021ba786cbb04435a7152f008b216c49.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const cors = require("cors");
-
+const path = require('path'); // Agregado
 
 const cats = require("./api/cats/cat.json");
 app.get('/cats', (req, res) => {
